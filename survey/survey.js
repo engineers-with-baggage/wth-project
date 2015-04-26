0cb961494a9c5b1f741e0db30c20adf4472814f7
Ryan Debeasi <rdebeasi@gmail.com>
2015-04-26 10:49:51 -0400

    Get form to generate the proper datazzz

--- survey/survey.js
+++ survey/survey.js
@@ -1,18 +1,34 @@
 $(function() {
+	var userContentStore = new Firebase('https://amber-inferno-2234.firebaseio.com/');
+	var geoDataStore = new Firebase('https://flickering-heat-4757.firebaseio.com/');
+
 	// Survey Form
 	$('#surveyForm').on('submit', function(e){
 		var $form = $(this),
-			data = {};
-		// var data = $(this).serializeJSON();
+			data = {},
+			originData;
 		e.preventDefault();
-		data.name = $('#name').val();
-		data.origin = {};
-		data.origin.latitude = "foo";
-		data.origin.longitude = "bar";
-		data.destination = {};
-		data.destination.latitude = "foo";
-		data.destination.longitude = "bar";
-		console.log(data);
+		// var data = $(this).serializeJSON();
+		geoDataStore.orderByChild('city').equalTo($('#origin').val()).once('value', function(snapshot) {
+			var originDataPrecursor = snapshot.val();
+			originData = originDataPrecursor[Object.keys(originDataPrecursor)[0]];
+			// This nesting is gross. I'm sorry.
+			geoDataStore.orderByChild('city').equalTo($('#destination').val()).once('value', function(snapshot) {
+				var destinationDataPrecursor = snapshot.val();
+				destinationData = destinationDataPrecursor[Object.keys(destinationDataPrecursor)[0]];
+				console.log('origin', originData);
+				console.log('destination', destinationData);
+				data.name = $('#name').val();
+				data.origin = {};
+				data.origin.latitude = originData.lat;
+				data.origin.longitude = originData.long;
+				data.destination = {};
+				data.destination.latitude = destinationData.lat;
+				data.destination.longitude = destinationData.long;
+				console.log(data);
+			});
+		});
+		
 	});
 
 });
