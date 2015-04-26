56c7d7bbf48d3370d96cf45c296f0a8d99c0bce1
Ryan Debeasi <rdebeasi@gmail.com>
2015-04-26 10:20:31 -0400

    zomg typos

--- survey/survey.js
+++ survey/survey.js
@@ -1,12 +1,18 @@
 $(function() {
 	// Survey Form
 	$('#surveyForm').on('submit', function(e){
-		var $form = $(this);
-		var data = $(this).serializeJSON();
+		var $form = $(this),
+			data = {};
+		// var data = $(this).serializeJSON();
 		e.preventDefault();
+		data.name = $('#name').val();
+		data.origin = {};
+		data.origin.latitude = "foo";
+		data.origin.longitude = "bar";
+		data.destination = {};
+		data.destination.latitude = "foo";
+		data.destination.longitude = "bar";
 		console.log(data);
 	});
-
-	// Autocomplete Stuff
 
 });
